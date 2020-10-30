c57907b5f2109276e0b46d73f78cbce69fd5fe4a
Juan Sebastian <juanrinconaxl926@gmail.com>
2020-10-29 19:08:33 -0500

    Modificacion de formulario de Adicion de Leads

--- index.php
+++ index.php
@@ -101,10 +101,10 @@ if ($_POST) {
                         <div class="form-group"><label class="small mb-1" for="inputEmailAddress">Usuario</label><input class="form-control py-4" id="inputEmailAddress" name="usuario" type="text" placeholder="Enter email address" /></div>
                         <div class="form-group"><label class="small mb-1" for="inputPassword">Password</label><input class="form-control py-4" id="inputPassword" name="password" type="password" placeholder="Enter password" /></div>
                         <div class="form-group">
-                          <div class="custom-control custom-checkbox"><input class="custom-control-input" id="rememberPasswordCheck" type="checkbox" /><label class="custom-control-label" for="rememberPasswordCheck">Remember password</label></div>
+                          <div class="custom-control custom-checkbox"><input class="custom-control-input" id="rememberPasswordCheck" type="checkbox" /><label class="custom-control-label" for="rememberPasswordCheck">Recordar Contraseña</label></div>
                         </div>
-                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"><a class="small" href="password.html">Forgot Password?</a>
-                          <button type="submit" class="btn btn-primary">Login</button></div>
+                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"><a class="small" href="password.html">¿Olvido La Contraseña?</a>
+                          <button type="submit" class="btn btn-primary">Iniciar Sesion</button></div>
                       </form>
                     </div>
                     </form>
