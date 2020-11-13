c1fb56a7dd7fdf57a401c9020d03a17c5beea8f6
Juan Sebastian <juanrinconaxl926@gmail.com>
2020-11-12 21:22:05 -0500

    Delete del crud y modificaciones sobre la gestion

--- Gestionalead.php
+++ Gestionalead.php
@@ -437,8 +437,9 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
                               echo "<td>";
                               echo $mostrar['asignado'];
                               echo "</td>";
-                              echo "<colspan='7'><div class='btn-group'><th><button type='button' class='btn btn-sm btn-primary'>Modificar</button></a><a href=''><button type='button' class='btn btn-sm btn-danger'>Eliminar</button></a>
-                              <a href=''><button type='button' class='btn btn-sm btn-success'>Convertir en cliente</button></a>
+                              echo "<colspan='7'><div class='btn-group'><th><a href='modificar.php'><button type='button' class='btn btn-sm btn-primary'>Modificar</button></a>
+                                                                            <a href='borrarlead.php'><button type='button' class='btn btn-sm btn-danger'>Eliminar</button></a>
+                                                                            <a href=''><button type='button' class='btn btn-sm btn-success'>Convertir en cliente</button></a>
                               </td>";
 
 
@@ -470,29 +471,40 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
             <!-- End of Page Wrapper -->
 
             <!-- Scroll to Top Button-->
-            <a class=" scroll-to-top rounded" href="#page-top">
+            <a class="scroll-to-top rounded" href="#page-top">
               <i class="fas fa-angle-up"></i>
             </a>
 
-            <!-- Logout Modal-->
-            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Cerrar Sesion</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">×</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">¿Desea Cerrar Sesion?</div>
-                  <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="index.php">Cerrar Sesion</a>
-                  </div>
-                </div>
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                  <?php echo $nombre;
+                  ?>
+                </span>
+                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profile
+                </a>
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Settings
+                </a>
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Activity Log
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Logout
+                </a>
               </div>
-            </div>
-
+            </li>
             <!-- Bootstrap core JavaScript-->
             <script src="vendor/jquery/jquery.min.js"></script>
             <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
