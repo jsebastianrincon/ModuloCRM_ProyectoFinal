23e7804d73d6d5062961b301faac3fe5418d8bd4
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-01-19 22:29:38 -0500

    Correciones

--- prueba.php
+++ prueba.php
@@ -1,6 +1,27 @@
- <!-- Tab links -->
- <ul class="nav">
-   <li class="nav-one"><a href="#contenido1" class="current">Pestaña 1</a></li>
-   <li class="nav-two"><a href="#contenido2">Pestaña 2</a></li>
-   <li class="nav-three"><a href="#contenido3">Pestaña 3</a></li>
- </ul>
+<?php
+
+require("conlead.php");
+
+
+$connexion = mysqli_connect('localhost', 'root', '', 'crmpry');
+
+mysqli_select_db($connexion, 'crmpry') or die("No se encuentra la Base de 
+ datos");
+$instruccion_SQL = "SELECT concat_ws (' ',nombre_lead , primer_apellido_lead) FROM leads WHERE estado_lead = 1 ORDER BY primer_apellido_lead";
+$resultado = mysqli_query($connexion, $instruccion_SQL);
+?>
+
+<html>
+
+<body>
+  <select>
+    <?php
+    while ($row = mysqli_fetch_array($resultado)) :;
+    ?>
+      <option value="<?php echo $row[0]; ?>"><?php echo $row[0]; ?></option>
+      <option value="<?php echo $row[0]; ?>"><?php echo $row[0]; ?></option>
+    <?php endwhile; ?>
+  </select>
+</body>
+
+</html>
