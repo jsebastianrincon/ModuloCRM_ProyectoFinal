3dbdfb34dafeb5891eadcf9226b2db3f7f3f729d
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-01-19 22:32:44 -0500

    Correciones sobre dashboard

--- principal.php
+++ principal.php
@@ -455,7 +455,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
             </div>
 
             <!-- Pending Requests Card Example -->
-            <div class="col-xl-2 col-md-6 mb-5">
+            <div class="col-xl-3 col-md-6 mb-5">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
