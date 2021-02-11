38747d6f86ba88da0e47b1df16026c9051f0eb56
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-02-10 22:34:18 -0500

    Correcciones sobre los registros de leads

--- RegistroLead.php
+++ RegistroLead.php
@@ -18,13 +18,7 @@ $compañia_lead = $_REQUEST["compañia_lead"];
 $asignado_lead = $_REQUEST["asignado_lead"];
 $recurso_lead = $_REQUEST["recurso_lead"];
 $comentario_lead = $_REQUEST["comentario_lead"];
-/*
-$lead_telefono = $_REQUEST["telefono"];
-$telefono_telefono = $_REQUEST["telefono_telefono"];
-$tipo_telefono = $_REQUEST["tipo_telefono"];
-$prioridad_telefono = $_REQUEST["prioridad_telefono"];
-$vigencia_telefono = $_REQUEST["vigencia_telefono"];
-*/
+
 require("conlead.php");
 $connexion = mysqli_connect('localhost', 'root', '', 'crmpry');
 
@@ -33,8 +27,9 @@ mysqli_select_db($connexion, 'crmpry') or die("No se encuentra la Base de
 
 $instruccion_SQL = "INSERT INTO leads (nombre_lead, segundo_nombre_lead,primer_apellido_lead,segundo_apellido_lead,tipodocumento_lead,documento_lead,telefono_lead,email_lead,departamento_lead,ciudad_lead,direccion_lead,estado_lead,compañia_lead,asignado_lead,recurso_lead,comentario_lead) VALUES ('$nombre_lead','$segundo_nombre_lead','$primer_apellido_lead','$segundo_apellido_lead','$tipodocumento_lead','$documento_lead','$telefono_lead','$email_lead','$departamento_lead','$ciudad_lead','$direccion_lead','0','$compañia_lead','$asignado_lead','$recurso_lead','$comentario_lead')";
 
-//$instruccion_SQL_telefono = "INSERT INTO telefono(lead_telefono ,telefono_telefono,tipo_telefono,prioridad_telefono,vigencia_telefono) VALUES ('$telefono','$nombre_lead','personal','1','1')";
+
 $resultado = mysqli_query($connexion, $instruccion_SQL);
+
 if ($resultado == FALSE) {
   echo "error en la consulta";
 } else {
@@ -42,6 +37,8 @@ if ($resultado == FALSE) {
   window.location.href= 'AñadirLead.php';
   </script>";
 }
+
+
 
 //header("Location: AñadirLead.php");
 
