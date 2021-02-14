e54370351eb22bd59c38c4728bb9c1b43839a8df
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-02-13 20:38:50 -0500

    Adicion funcionalidad reportes

--- AñadirLead.php
+++ AñadirLead.php
@@ -125,7 +125,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
 
         <!-- Nav Item - Tables -->
         <li class="nav-item">
-          <a class="nav-link" href="tables.html">
+          <a class="nav-link" href="reportes.php">
             <i class="fas fa-fw fa-table"></i>
             <span>Reportes</span></a>
         </li>
@@ -141,7 +141,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
 
         <!-- Nav Item - Tables -->
         <li class="nav-item">
-          <a class="nav-link" href="tables.html">
+          <a class="nav-link" href="reportes.php">
             <i class="fas fa-fw fa-table"></i>
             <span>Reportes</span></a>
         </li>
