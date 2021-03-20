ab36a0fe190bd0bab974cac627614fe7ba2e40a5
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-03-19 23:11:56 -0500

    Eliminacion funciones no utiles

--- gestionarclientes.php
+++ gestionarclientes.php
@@ -204,6 +204,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
+            <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
@@ -214,7 +215,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="verperfil.php">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Perfil
                 </a>
