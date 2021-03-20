10012c4901443a3ec2af560ab9acf24729a4e298
Juan Sebastian <juanrinconaxl926@gmail.com>
2021-03-19 22:20:13 -0500

    Adicion informacion de contacto

--- RegistroContacto.php
+++ RegistroContacto.php
@@ -1,5 +1,5 @@
 <?php
-
+$id_lead = $_REQUEST["id_lead"];
 $lead_telefono = $_REQUEST["lead_telefono"];
 $telefono_telefono = $_REQUEST["telefono_telefono"];
 $tipo_telefono = $_REQUEST["tipo_telefono"];
@@ -12,7 +12,7 @@ mysqli_select_db($connexion, 'crmpry') or die("No se encuentra la Base de
  datos");
 
 
-$instruccion_SQL_telefono = "INSERT INTO telefonos (lead_telefono,telefono_telefono,tipo_telefono,priorida_telefono,vigencia_telefono) VALUES ('$lead_telefono','$telefono_telefono','$tipo_telefono','1','1')";
+$instruccion_SQL_telefono = "INSERT INTO telefonos (id_lead,lead_telefono,telefono_telefono,tipo_telefono,priorida_telefono,vigencia_telefono) VALUES ('$id_lead','$lead_telefono','$telefono_telefono','$tipo_telefono','1','1')";
 
 
 $resultado_telefono = mysqli_query($connexion, $instruccion_SQL_telefono);
